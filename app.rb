c2a439b96de26f5c0615880be0d4010de77335f9
Duncan Fedde <duncan@fedde.us>
2014-11-26 14:07:57 -0700

    Uses a session for name
    This allows a user to only have to put in a name once

--- app.rb
+++ app.rb
@@ -1,14 +1,25 @@
 require "sinatra"
 require 'sinatra/cross_origin'
+
 set port: 4567
 set bind: '0.0.0.0'
 configure do
     enable :cross_origin
 end
 
-get '/done/:lab/:name' do
+use Rack::Session::Cookie, :key => 'done.user',
+                           :path => '/',
+                           :expire_after => 2592000,
+                           :secret => 'o93ci49ci38283icrkric040393kidie83kidi303kcxasldeped3'
+
+post '/login' do
+  session[:name] = params[:name]
+  "thank you for logging in #{session[:name]}"
+end
+
+get '/done_with/:lab' do
   open('finished', 'a') { |f|
-    f.puts "#{params[:name]} finished lab #{params[:lab]} at #{Time.now.strftime("at %I:%M%p")}"
+    f.puts "#{session[:name]} finished lab #{params[:lab]} at #{Time.now.strftime("at %I:%M%p")}"
   }
-  "thank you, #{params[:name]}"
+  "thank you, #{session[:name]}"
 end
